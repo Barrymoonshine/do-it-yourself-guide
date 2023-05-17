df3bedf97235616dccfb74403dc55c2247c07d27
Nick Sheward <nickshwrd@gmail.com>
2023-05-17 16:12:43 +0100

    Tidied up create React app boiler plate

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
   return (
@@ -7,8 +8,13 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <h1>Welcome to my first React app!</h1>
         </p>
+        <MyButton />
+        <AboutPage name="Nick" heroName="Spiderman">
+          <p> This is children props</p>
+        </AboutPage>
+        <Shoppinglist />
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -21,5 +27,47 @@ function App() {
     </div>
   );
 }
+
+const MyButton = () => {
+  const [count, setCount] = useState(0);
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+
+  return <button onClick={handleClick}>Clicked {count} times</button>;
+};
+
+const AboutPage = (props) => (
+  <>
+    <h1>About</h1>
+    <p className="my-first-class">
+      Heeellooo there {props.name} a.k.a {props.heroName}
+      {props.children}
+      <br />
+      How do you do?
+    </p>
+  </>
+);
+
+const products = [
+  { title: "Cabbage", id: 1 },
+  { title: "Garlic", id: 2 },
+  { title: "Apple", id: 3 },
+];
+
+const Shoppinglist = () => {
+  const listItems = products.map((product) => (
+    <li
+      key={product.id}
+      style={{
+        color: product.isFruit ? "magenta" : "darkgreen",
+      }}
+    >
+      {product.title}
+    </li>
+  ));
+
+  return <ul>{listItems}</ul>;
+};
 
 export default App;
