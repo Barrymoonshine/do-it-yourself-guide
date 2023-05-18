e601a3e58369d9dea8613ceb07de821ab6ac805f
Nick Sheward <nickshwrd@gmail.com>
2023-05-18 11:57:04 +0100

    Created changeTask method in Overview.js

--- src/App.js
+++ src/App.js
@@ -6,16 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <p>
-          <h1>Welcome to my first React app - the Tasks Transpiler!</h1>
-        </p>
-        <form>
-          <label for="New task">Task:</label>
-          <input type="text" id="task" name="task" minlength="1" required />
-          <button id="submit-task-button" type="submit">
-            Submit
-          </button>
-        </form>
+        Welcome to my first React app - the Tasks Transpiler!
         <Overview />
       </div>
     );
--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,8 +1,35 @@
 import React, { Component } from "react";
 
 class Overview extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      task: "",
+    };
+  }
+
+  changeTask = (event) => {
+    this.setState({
+      task: event.target.value,
+    });
+  };
+
   render() {
-    return <h1>This is a Class Component</h1>;
+    return (
+      <div>
+        <form>
+          <label>New task:</label>
+          <input
+            type="text"
+            value={this.state.task}
+            onChange={this.changeTask}
+          />
+          <button id="submit-task-button" type="submit">
+            Submit
+          </button>
+        </form>
+      </div>
+    );
   }
 }
 
