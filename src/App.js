a89f1720d3a21dbe6c9fc532139464b68c01a95c
Nick Sheward <nickshwrd@gmail.com>
2023-05-22 09:21:34 +0100

    Refactored code to pass props from adult App.js to child Overview.js component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,45 @@ import Overview from "./components/Overview";
 import React, { Component } from "react";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      task: "",
+      displayedTasks: [],
+    };
+  }
+
+  changeTask = (event) => {
+    this.setState({
+      task: event.target.value,
+    });
+  };
+
+  resetForm() {
+    this.setState({
+      task: "",
+    });
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.setState({
+      displayedTasks: [...this.state.displayedTasks, this.state.task],
+    });
+    this.resetForm();
+  };
+
   render() {
+    const { task, displayedTasks } = this.state;
     return (
-      <div className="App">
-        Welcome to my first React app - the Tasks Transpiler!
-        <Overview />
+      <div className="TaskContainer">
+        <form onSubmit={this.handleSubmit}>
+          <label>New task:</label>
+          <input type="text" value={task} onChange={this.changeTask} />
+          <button type="submit">Submit</button>
+        </form>
+        <p> My tasks:</p>
+        <Overview tasks={displayedTasks} />
       </div>
     );
   }
--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,50 +1,14 @@
 import React, { Component } from "react";
 
 class Overview extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      task: "",
-      displayedTasks: [],
-    };
-  }
-
-  changeTask = (event) => {
-    this.setState({
-      task: event.target.value,
-    });
-  };
-
-  resetForm() {
-    this.setState({
-      task: "",
-    });
-  }
-
-  handleSubmit = (event) => {
-    event.preventDefault();
-    this.setState({
-      displayedTasks: [...this.state.displayedTasks, this.state.task],
-    });
-    this.resetForm();
-  };
-
   render() {
-    const { task, displayedTasks } = this.state;
+    const { tasks } = this.props;
     return (
-      <div>
-        <form onSubmit={this.handleSubmit}>
-          <label>New task:</label>
-          <input type="text" value={task} onChange={this.changeTask} />
-          <button type="submit">Submit</button>
-        </form>
-        <p> My tasks:</p>
-        <ul>
-          {displayedTasks.map((task, index) => (
-            <li key={index}>{task}</li>
-          ))}
-        </ul>
-      </div>
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>{task}</li>
+        ))}
+      </ul>
     );
   }
 }
